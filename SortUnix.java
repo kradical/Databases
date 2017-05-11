c3d0eb10c1fe1214e54489d8e248d4e5a9f45928
Konrad Schultz <konrad_here@hotmail.com>
2017-05-11 12:11:57 -0700

    initial template code

---
+++ SortUnix.java
@@ -0,0 +1,12 @@
+public class SortUnix {
+
+	public static void main(String[] args) throws Exception {
+		long startTime = System.currentTimeMillis();
+		
+        System.out.println("Sorting... be patient");
+		//Remove ".exe" for Mac and Linux
+        Runtime.getRuntime().exec("sort.exe -k 2 taxpayers_3M.txt -o taxpayers_3M_sort.txt").waitFor();
+        
+		System.out.println("Time elapsed (sec) = " + (System.currentTimeMillis() - startTime) / 1000.0);
+	}
+}
